4aec845a7c24fdf08f7fced1c623dd3c25b13b4a
Eduardo M. Santos <contact@supitsdu.me>
2024-11-24 01:21:40 -0300

    chore(rollup-config): update config

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,56 +2,30 @@ import typescript from "@rollup/plugin-typescript"
 import dts from "rollup-plugin-dts"
 import esbuild from "rollup-plugin-esbuild"
 
-import { reportDetails } from "./src/main"
-
 const external = ["node:path", "node:zlib", "node:util"]
 
 export default [
 	{
 		input: ["src/main.ts"],
-		plugins: [typescript(), reportDetails()],
-		output: {
-			dir: "build",
-			format: "es",
-			exports: "auto",
-		},
+		plugins: [typescript()],
+		output: { dir: "build", format: "es", exports: "auto" },
 		external,
 	},
 	{
 		input: "build/main.js",
 		plugins: [
-			esbuild({
-				minify: true,
-				target: "esnext",
-				minifyIdentifiers: true,
-				minifySyntax: true,
-				minifyWhitespace: true,
-			}),
-			reportDetails(),
+			esbuild({ minify: true, target: "esnext", minifyIdentifiers: true, minifySyntax: true, minifyWhitespace: true }),
 		],
 		output: [
-			{
-				file: "dist/main.cjs",
-				format: "cjs",
-				exports: "auto",
-			},
-			{
-				file: "dist/main.mjs",
-				format: "esm",
-				exports: "auto",
-			},
+			{ file: "dist/main.cjs", format: "cjs", exports: "auto" },
+			{ file: "dist/main.mjs", format: "esm", exports: "auto" },
 		],
 		external,
 	},
-
 	{
 		input: "build/src/main.d.ts",
-		plugins: [dts(), reportDetails()],
-		output: {
-			file: "dist/main.d.ts",
-			format: "esm",
-			exports: "auto",
-		},
+		plugins: [dts()],
+		output: { file: "dist/main.d.ts", format: "esm", exports: "auto" },
 		external,
 	},
 ]
