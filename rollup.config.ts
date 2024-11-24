938ac924da9996ac69b09282f791e83744d8e736
Eduardo M. Santos <contact@supitsdu.me>
2024-11-24 00:27:52 -0300

    chore: upload initial project setup

---
+++ rollup.config.ts
@@ -0,0 +1,57 @@
+import typescript from "@rollup/plugin-typescript"
+import dts from "rollup-plugin-dts"
+import esbuild from "rollup-plugin-esbuild"
+
+import { reportDetails } from "./src/main"
+
+const external = ["crypto", "path", "node:zlib", "node:util"]
+
+export default [
+	{
+		input: ["src/main.ts"],
+		plugins: [typescript(), reportDetails()],
+		output: {
+			dir: "build",
+			format: "es",
+			exports: "auto",
+		},
+		external,
+	},
+	{
+		input: "build/main.js",
+		plugins: [
+			esbuild({
+				minify: true,
+				target: "esnext",
+				minifyIdentifiers: true,
+				minifySyntax: true,
+				minifyWhitespace: true,
+			}),
+			reportDetails(),
+		],
+		output: [
+			{
+				file: "dist/main.cjs",
+				format: "cjs",
+				exports: "auto",
+			},
+			{
+				file: "dist/main.mjs",
+				format: "esm",
+				exports: "auto",
+			},
+		],
+		external,
+	},
+
+	{
+		input: "build/src/main.d.ts",
+		plugins: [dts(), reportDetails()],
+		output: {
+			file: "dist/main.d.ts",
+			format: "esm",
+			exports: "auto",
+		},
+		external,
+	},
+]
